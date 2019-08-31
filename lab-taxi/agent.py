22c5ff2883a3246273ec7e30d3bb3d3915ddcf6b
Yousof <yousofebneddin@yahoo.com>
2019-08-31 15:43:26 -0700

    my solution to taxi_v2 problem

--- lab-taxi/agent.py
+++ lab-taxi/agent.py
@@ -1,5 +1,6 @@
 import numpy as np
 from collections import defaultdict
+import random
 
 class Agent:
 
@@ -12,6 +13,11 @@ class Agent:
         """
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.policy = None
+        self.eps = 1
+        self.i_episode = 1
+        self.gamma = 0.8
+        self.alpha = 0.1
 
     def select_action(self, state):
         """ Given the state, select an action.
@@ -24,7 +30,7 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
-        return np.random.choice(self.nA)
+        return self.action_epsilon_greedy(state)
 
     def step(self, state, action, reward, next_state, done):
         """ Update the agent's knowledge, using the most recently sampled tuple.
@@ -37,4 +43,30 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        self.Q[state][action] += 1
+        old_value = self.Q[state][action]
+        if done:
+            self.Q[state][action] = old_value + self.alpha * (reward - old_value)
+            self.update_policy_from_q_greedy()
+            self.end_of_episode()
+            return
+        self.Q[state][action] = old_value + \
+            self.alpha * (reward + self.gamma * np.max(self.Q[next_state]) - old_value)
+        self.update_policy_from_q_greedy()
+        
+
+    def update_policy_from_q_greedy(self):
+        self.policy = dict((k,np.argmax(v)) for k, v in self.Q.items())
+    
+    def action_epsilon_greedy(self, state):
+        if random.random()< self.eps or self.policy == None or state not in self.policy:
+            action = np.random.choice(np.arange(self.nA))
+        else:
+            action = self.policy[state]
+        return action
+
+    def end_of_episode(self):
+        self.i_episode += 1
+        self.eps = 1 / self.i_episode
+        #self.eps = max (np.interp(self.i_episode,[1.0,5000.0], [1, 0.1]) , 0.000001)
+        self.prev_state = None
+        self.prev_action = None
--- lab-taxi/main.py
+++ lab-taxi/main.py
@@ -4,5 +4,5 @@ import gym
 import numpy as np
 
 env = gym.make('Taxi-v2')
-agent = Agent()
-avg_rewards, best_avg_reward = interact(env, agent)
+agent = Agent(nA = env.action_space.n)
+avg_rewards, best_avg_reward = interact(env, agent, num_episodes=20000)
